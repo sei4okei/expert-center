8a56ee858d5f9c4a536fd0e0e0fd5f2ac9b4c1e1
admin <sei.4okei@yandex.ru>
2024-07-09 00:16:34 +0300

    layered architecture

--- ExpertCenter/ExpertCenter/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ExpertCenter.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
